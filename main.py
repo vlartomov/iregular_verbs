3718abd7d0811070579a67cdf42ce4254bd8bac9
vladimir <vlartomov@gmail.com>
2021-10-09 22:20:47 +0300

    commited a lot of lines

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 #a = [str(i) for i in input().split()]
-list = open("words.txt", "r")
-words = []
-for k in list:
-    striped_line = k.strip()
-    line_list = striped_line.split()
-    words.append(line_list)
-list.close()
-words1 = []
-count = 0
-for x in words:
-    print(x[0])
-    print("Please enter answer: ")
-    while count != 3:
-        inp1 = input()
-        words1.append(inp1)
-        count += 1
-    print(words1)
+#list = open("words.txt", "r")
+#words = []
+#for k in list:
+#    striped_line = k.strip()
+#    line_list = striped_line.split()
+#    words.append(line_list)
+#list.close()
+#words1 = []
+#count = 0
+#for x in words:
+#    print(x[0])
+#    print("Please enter answer: ")
+#    while count != 3:
+#        inp1 = input()
+#       words1.append(inp1)
+#        count += 1
+#    print(words1)
 
     for m in inp:
        striped_line1 = m.strip()
